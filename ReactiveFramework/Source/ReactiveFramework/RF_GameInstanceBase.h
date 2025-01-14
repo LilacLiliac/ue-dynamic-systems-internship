7d653674bee4b3580b6943554662a0174ac9fb4c
jasonfilips <jasonfilipss@yahoo.com>
2025-01-14 12:28:40 +0200

    Add base classes
    Added base classes with better naming to underline they are base classes

---
+++ ReactiveFramework/Source/ReactiveFramework/RF_GameInstanceBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyGameInstance.h"
+#include "RF_GameInstanceBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REACTIVEFRAMEWORK_API URF_GameInstanceBase : public UMyGameInstance
+{
+	GENERATED_BODY()
+	
+};
